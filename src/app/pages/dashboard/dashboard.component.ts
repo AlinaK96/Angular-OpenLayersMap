1e4675d6862136c02240b8aa99efde355b1a6ecc
Alina <AliKochneva@yandex.ru>
2023-08-28 21:36:08 +0700

    добавление стартовых координат

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +13,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  title = 'Map Viewer - Openlayers & Angular'; 
+  map: Map = new Map;
 
+  ngOnInit(): void {
+    this.map = new Map({
+      view: new View({
+        center : fromLonLat([ 86.0833 ,  55.3333]),
+        zoom: 12,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'ol-map'
+    });
+  }
 }
